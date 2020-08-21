82f4b99a41a30807b8b4ad0818060b51dc2c3c45
Duy <ddtuan01@gmail.com>
2020-08-22 02:51:22 +0700

    update test.py

--- Project 1/Source/test.py
+++ Project 1/Source/test.py
@@ -9,21 +9,18 @@ from sklearn import metrics
 
 # read command line arguments
 argv_len = len(sys.argv)
-X_path = sys.argv[1] if argv_len > 1 else "X_train.csv"
-Y_path = sys.argv[2] if argv_len > 2 else "Y_train.csv"
-out_model_path = sys.argv[3] if argv_len > 3 else "output.model"
+out_model_path = sys.argv[1] if argv_len > 1 else "test.model"
+X_path = sys.argv[2] if argv_len > 2 else "X.csv"
+Y_path = sys.argv[3] if argv_len > 3 else "predictY.csv"
 
 # read data
 dataX = pd.read_csv(X_path)
-dataY = pd.read_csv(Y_path)
 del dataX['id']
-del dataY['id']
 
 # pre-process
 # handle missing values
 NaNRows = dataX[dataX.isna().any(axis=1)]
 dataX.drop(index=NaNRows.index, errors='ignore', inplace=True)
-dataY.drop(index=NaNRows.index, errors='ignore', inplace=True)
 
 # delete duplicates
 # duplicateRows = dataX[dataX.duplicated(keep='first')]
@@ -52,14 +49,14 @@ for col in enc_cols.columns:
 dataX.drop(columns=ctg_cols, inplace=True)
 
 # test
-def test(X_test, Y_test):
+def test(X_test):
     # load_model()
     with open(out_model_path, 'rb') as file:
         model = pickle.load(file)
-    #
-    Y_pred = model.predict(X_test)
-    rmse = np.sqrt(metrics.mean_squared_error(Y_test, Y_pred))
-    return rmse;
 
-# RMSE
-print("RMSE:", test(dataX, dataY))
+    Y_pred = model.predict(X_test)
+
+    # save Y_pred
+    np.savetxt(Y_path, Y_pred, delimiter=",")
+
+test(dataX)
